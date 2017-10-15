3f55afad8cdf0dc7b571e313c96930d5751213c2
alex-rokabilis <alekos_l2@hotmail.com>
2017-10-15 13:58:27 +0300

    test

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const Led = require('./LED')
 
-
-
 const timeout = ms => new Promise(res => setTimeout(res, ms))
+const red = new Led(13)
+const green = new Led(12)
+const blue = new Led(18)
 
-async function delay () {
+
+async function main() {
     console.log('started')
-    await timeout(5000)
+    red.off()
+    green.off()
+    blue.off()
+    
+    await timeout(500)
+    
+    red.on()
+    await timeout(500)
+    red.off()
+    
+    blue.on()
+    await timeout(500)
+    blue.off()
+    
+    green.on()
+    await timeout(500)
+    green.off()
+    
+    
     console.log('finished')
-  }
-  
-  delay()
+}
+
+delay()
