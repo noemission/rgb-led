c250056130435247f883afe04e1e17611698169f
alex-rokabilis <alekos_l2@hotmail.com>
2017-10-15 14:44:54 +0300

    readline interface

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const Led = require('./LED')
+const readline = require('readline');
+const Color = require('color');
 
-const timeout = ms => new Promise(res => setTimeout(res, ms))
+
+//initialization
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 const red = new Led(13)
 const green = new Led(12)
 const blue = new Led(18)
+
+//utilities
+const timeout = ms => new Promise(res => setTimeout(res, ms))
+const flush = () => {
+    red.off();
+    green.off();
+    blue.off();
+}
+
+
+//cmd questions
+function cmd() {
+    rl.question('Give me a color: ', (answer) => {
+
+        try {
+            var color = new Color(answer)
+        } catch (err) {
+            console.log(`Sorry ${answer} is not a valid color!`);
+            return cmd();
+        }
+
+        red.rgb(color.red())
+        green.rgb(color.green())
+        blue.rgb(color.blue())
+        //console.log(`Thank you for your valuable feedback: ${answer}`);
+
+        cmd()
+    });
+}
 
 
 async function test() {
@@ -30,20 +66,15 @@ async function test() {
     console.log('test finished')
 }
 
-async function main() {
+
+
+
+async (function main() {
     await test();
+    cmd()
+})()
 
-    red.rgb(52);
-    green.rgb(179);
-    blue.rgb(135);
 
-    await timeout(50000)
-}
 
-main()
 
-process.on('SIGINT', () => {
-    red.off()
-    green.off()
-    blue.off()
-});
+process.on('SIGINT', flush);
